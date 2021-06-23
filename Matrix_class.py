d8b6078375f98d1416ef9f36217b5b2cd8f78bc6
Benthijs <ben.mason@hotmail.nl>
2021-06-23 20:10:20 +0200

    We added a function to change a random number of bits in a message

--- Matrix_class.py
+++ Matrix_class.py
@@ -1,3 +1,5 @@
+import random
+
 class matrix:
     """Represents a matrix with elements mod 2
     Attributes:
@@ -77,9 +79,78 @@ class matrix:
                 new_matrix[i][j] = (self.get(i, j) + other.get(i, j)) % 2
         return new_matrix
     
+vector = G * p
 
 
-A = matrix([[1, 1, 1], [1, 1, 1], [1, 1, 1]])
-B = matrix([[1], [1], [1]])
+G = matrix([[1, 1, 0, 1], [1, 0, 1, 1], [1, 0, 0, 0], [0, 1, 1, 1], [0, 1, 0, 0],[0, 0, 1, 0], [0, 0, 0, 1]])
+G_T = matrix([[1, 1, 1, 0, 0, 0, 0], [1, 0, 0, 1, 1, 0, 0], [0, 1, 0, 1, 0, 1, 0], [1, 1, 0, 1, 0, 0, 1]])
+H = matrix([[1, 0, 1, 0, 1, 0, 1], [0, 1, 1, 0, 0, 1, 1], [0, 0, 0, 1, 1, 1, 1]])
+p = matrix([[1], [0], [1], [1]])
+R = matrix([[0,0,1,0,0,0,0], [0,0,0,0,1,0,0],[0,0,0,0,0,1,0],[0,0,0,0,0,0,1]])
+       
 
-print(A*B)
+def omzet_bits(vector):
+    """Switches a random number of bits"""
+    prob = random.random() 
+    bits = vector.mat 
+    for i in range(len(bits)):
+        if random.random() < prob:
+            bits[i] = (bits[i] + 1) % 2
+    return vector
+
+
+#TODO check dimension of vector
+def codeer(vector):
+    """Codes the 4 bit message into one with three parity bits
+        Attributes:
+            vector: the 4-bit message"""
+    return(G * vector)
+
+def decodeer(vector):
+    """Decodes the 7 bit vector into the original 4-bit message
+        Attributes
+            vector: the 7-bit hamming code with 3 parity and 4 data bits"""
+    return R * vector
+
+def controleer(vector):
+    """Returns True if the message is sent correctly according to the parity bits"""
+    
+    #We moeten checken of de matrix H verminigvuldigd met een vector 0 is.
+    controleer_vector = H * vector
+    #Een vector bestaande uit 0 en 1 is 0 d.e.s.d.a. de som van de componenten 0 is.
+    som_vector = 0
+    
+    for i in range(len(controleer_vector.rows)):
+        som_vector += sum(controleer_vector.rows[i])
+        if som_vector == 0:
+            return True #Geen fout opgetreden
+        else:
+            return False #fout opgetreden
+  
+            
+# TODO implement error correction function
+
+def corrigeren(vector):
+    """Returns the corrected vector based on the Hamming(7-4) code"""
+    if(controleer(vector)):
+        return vector
+    # see error correction Hamming(7,4) wikipedia
+
+
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
