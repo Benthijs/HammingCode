a0c7aacb701b80f806dc2d78566d82b599c995a6
Benthijs <ben.mason@hotmail.nl>
2021-07-02 13:31:27 +0200

    Amin - Added support for the Hamming 11-7 code

--- hamming.py
+++ hamming.py
@@ -1,19 +1,40 @@
 from Matrix_class import matrix
 import random
 
+"----- Hamming 7, 4 --------"
 
 G = matrix([[1, 1, 0, 1], [1, 0, 1, 1], [1, 0, 0, 0], [0, 1, 1, 1], [0, 1, 0, 0],[0, 0, 1, 0], [0, 0, 0, 1]])
-G_T = matrix([[1, 1, 1, 0, 0, 0, 0], [1, 0, 0, 1, 1, 0, 0], [0, 1, 0, 1, 0, 1, 0], [1, 1, 0, 1, 0, 0, 1]])
 H = matrix([[1, 0, 1, 0, 1, 0, 1], [0, 1, 1, 0, 0, 1, 1], [0, 0, 0, 1, 1, 1, 1]])
-p = matrix([[1], [0], [1], [1]])
 R = matrix([[0,0,1,0,0,0,0], [0,0,0,0,1,0,0],[0,0,0,0,0,1,0],[0,0,0,0,0,0,1]])
-vector = G * p   
+
+"----- Hamming 11,7 --------"
+
+G2 = matrix([[1, 1, 0, 1, 1, 0, 1],
+             [1, 0, 1, 1, 0, 1, 1],
+             [1, 0, 0, 0, 0, 0, 0],
+             [0, 1, 1, 1, 0, 0, 0],
+             [0, 1, 0, 0, 0, 0, 0],
+             [0, 0, 1, 0, 0, 0, 0],
+             [0, 0, 0, 1, 0, 0, 0],
+             [0, 0, 0, 0, 1, 1, 1],
+             [0, 0, 0, 0, 1, 0, 0],
+             [0, 0, 0, 0, 0, 1, 0],
+             [0, 0, 0, 0, 0, 0, 1]])
+H2 = matrix([[1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+             [0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1],
+             [0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1]])
+R2 = matrix([[0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]])
+
 BASIS = [matrix([[1], [0], [0], [0], [0], [0], [0]]), matrix([[0], [1], [0], [0], [0], [0], [0]]), matrix([[0], [0], [1], [0], [0], [0], [0]]),
          matrix([[0], [0], [0], [1], [0], [0], [0]]), matrix([[0], [0], [0], [0], [1], [0], [0]]), matrix([[0], [0], [0], [0], [0], [1], [0]]),
          matrix([[0], [0], [0], [0], [0], [0], [1]]),]
-
-vector_fout = matrix([[0], [1], [1], [0], [1], [1], [1]])
-
 
 
 
@@ -21,19 +42,30 @@ def codeer(vector):
     """Codes the 4 bit message into one with three parity bits
         Attributes:
             vector: a matrix object representing the 4-bit message"""
-    return(G * vector)
+    if vector.dim[0] == 4:
+        return(G * vector)
+    if vector.dim[0] == 7:
+        return(G2 * vector)
 
 def controleer(vector):
     """Returns True if the message is sent correctly according to the parity bits"""
     #We moeten checken of de matrix H verminigvuldigd met een vector 0 is.
-    controleer_vector = H * vector
-    #Een vector bestaande uit 0 en 1 is 0 d.e.s.d.a. de som van de componenten 0 is.
-    for i in range(len(controleer_vector.rows)):
-        component = sum(controleer_vector.rows[i])
-        if component % 2 != 0:
-            return False #fout opgetreden
-    return True #geen fout opgetreden
-      
+    if vector.dim[0] == 7:
+        controleer_vector = H * vector
+    
+        for i in range(len(controleer_vector.rows)):
+            component = sum(controleer_vector.rows[i])
+            if component != 0:
+                return False #fout opgetreden
+            return True #geen fout opgetreden
+    if vector.dim[0] == 11:
+        controleer_vector = H2 * vector
+        for i in range(len(controleer_vector.rows)):
+            component = sum(controleer_vector.rows[i])
+            if component != 0:
+                return False
+            return True
+        
 def corrigeren(vector):
     """Returns the corrected vector based on the Hamming(7-4) code
     Attributes:
@@ -60,6 +92,17 @@ def decodeer(vector):
     """Decodes the 7 bit vector into the original 4-bit message
         Attributes
             vector: matrix object representing the 7-bit hamming code with 3 parity"""
-    vector = corrigeren(vector)
-    return R * vector
+    if vector.dim[0] == 11:
+        return R2 * vector
+    if vector.dim[0] == 7:
+        vector = corrigeren(vector)
+        return R * vector
 # bitwise hamming(7-4) code
+
+
+
+
+
+
+
+
---
+++ snelheid_vergelijking.py
@@ -0,0 +1,46 @@
+"""
+We compare the runtime of the bitwise hamming implementation with the matrix
+version
+"""
+import time
+import math
+import hamming
+import bitwise_hamming
+import hamming_unittests
+
+
+def hamming_functions(vector):
+    """Uses the hamming functions to execute a couple of tasks"""
+    hamming_vector = hamming.codeer(vector)
+    hamming.decodeer(hamming_vector)
+    # correct an error
+    wrong_vector = hamming_unittests.omzet_bits(hamming_vector, 1)
+    hamming.decodeer(wrong_vector)
+
+def bitwise_hamming_functions(vector):
+    """Executes the same tasks as in hamming_functions only uses the bitwise
+    implementation"""
+    hamming_vector = bitwise_hamming.bitwise_codeer(vector)
+    bitwise_hamming.bitwise_decodeer(hamming_vector)
+    # correct the vector
+    wrong_vector = hamming_unittests.omzet_bits(hamming_vector, 1)
+    bitwise_hamming.bitwise_decodeer(wrong_vector)
+
+def compare(number = 10000):
+    """Compares the two versions of the hamming code we created
+        Attributes:
+            number: number of times each function is executed
+            """
+    vector = hamming_unittests.random_vector()
+    matrix_time = time.time()
+    for _ in range(number):
+        hamming_functions(vector)
+    matrix_time = time.time() - matrix_time
+    bitwise_time = time.time()
+    for _ in range(number):
+        bitwise_hamming_functions(vector)
+    bitwise_time = time.time() - bitwise_time
+    normalization = max([matrix_time, bitwise_time])
+    print(("Time of execution for: \n matrix hamming code tasks: {:.2%} \n" +
+           "bitwise hamming tasks: {:.2%}").format(matrix_time/normalization,
+                                                   bitwise_time/normalization))
